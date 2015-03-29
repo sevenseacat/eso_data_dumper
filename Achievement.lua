2999ca2c346de7e4d3384eeca80946ba37f1d772
Rebecca Skinner <traybaby@gmail.com>
2015-03-29 17:34:51 +0800

    Add nextInLine achievement ID, and a basic README and TODO file

--- Achievement.lua
+++ Achievement.lua
@@ -9,12 +9,17 @@ function Achievement.new(id)
   self.name,
   self.description,
   self.points = GetAchievementInfo(self.id)
+  self.nextInLine = GetNextAchievementInLine(self.id)
 
   return self
 end
 
 function Achievement:toDump()
   achievement = { name = self.name, description = self.description, points = self.points }
+
+  if self.nextInLine ~= 0 then
+    achievement["nextInLine"] = self.nextInLine
+  end
 
   return achievement
 end
